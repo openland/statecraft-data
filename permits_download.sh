58147ffd3042ff00161a4292cd55ac54943468eb
Steve Kite <korshakov.stepan@gmail.com>
2017-11-21 22:32:04 +0300

    Added 2015 year to issued permits

---
+++ permits_download.sh
@@ -0,0 +1,13 @@
+set -e
+wget -O permits_issued_raw/January2015.xlsx http://sfdbi.org/sites/default/files/January2015Issued.xlsx
+wget -O permits_issued_raw/February2015.xlsx http://sfdbi.org/sites/default/files/February2015Issued.xlsx
+wget -O permits_issued_raw/March2015.xlsx http://sfdbi.org/sites/default/files/March2015Issued.xlsx
+wget -O permits_issued_raw/April2015.xlsx http://sfdbi.org/sites/default/files/April2015Issued.xlsx
+wget -O permits_issued_raw/May2015.xlsx http://sfdbi.org/sites/default/files/May2015%20Issued%20Permits.xlsx
+wget -O permits_issued_raw/June2015.xlsx http://sfdbi.org/sites/default/files/June2015Issued.xlsx
+wget -O permits_issued_raw/July2015.xlsx http://sfdbi.org/sites/default/files/July2015Issued.xlsx
+wget -O permits_issued_raw/August2015.xlsx http://sfdbi.org/sites/default/files/August2015Issued.xlsx
+wget -O permits_issued_raw/September2015.xlsx http://sfdbi.org/sites/default/files/September2015Issued.xlsx
+wget -O permits_issued_raw/October2015.xlsx http://sfdbi.org/sites/default/files/October2015Issued.xlsx
+wget -O permits_issued_raw/November2015.xlsx http://sfdbi.org/sites/default/files/November2015Issued.xlsx
+wget -O permits_issued_raw/December2015.xlsx http://sfdbi.org/sites/default/files/December2015Issued.xlsx
