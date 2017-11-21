c792b2ada80de45b54acab012ff9899dbe1d2dae
Steve Kite <korshakov.stepan@gmail.com>
2017-11-21 22:22:48 +0300

    Added 2004-2010 years to issued permits

--- permits_issued.sh
+++ permits_issued.sh
@@ -1,6 +1,16 @@
+rm -fr permits_issued
+mkdir permits_issued
+
+for i in permits_issued_raw/*.xls;
+ do
+  filename=$(basename "$i" .xls);
+  outext=".csv" 
+  ssconvert $i permits_issued/$filename$outext
+done
+
 for i in permits_issued_raw/*.xlsx;
  do
   filename=$(basename "$i" .xlsx);
   outext=".csv" 
-  xlsx2csv $i $filename$outext
+  ssconvert $i permits_issued/$filename$outext
 done
